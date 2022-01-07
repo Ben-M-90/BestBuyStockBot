90d18dec5b07626ae786606199a9584e4e772f67
Ben-M-90 <ben.miller.90.bm@gmail.com>
2022-01-07 12:24:48 -0500

    Code refactoring/cleanup: - Renamed variables - Sort import list - Remove unused variables - Fix Selenium 'find_element_by_*' changing to 'find_element(By.*, "")' - Remove uneccessary comments - Webhook only created if discord printing is turned on.
    Bug fixes:
    - Code not accounting for 'Check Nearby' button, false flagging as "In Stock"
    - Update button locator references (Best Buy renamed buttons, breaking previous links)

--- BestBuyStockBot/BestBuyStockBot.py
+++ BestBuyStockBot/BestBuyStockBot.py
@@ -1,56 +1,46 @@
+from decimal import Decimal
+from discord import Webhook, RequestsWebhookAdapter
+from dotenv import load_dotenv
+from fake_useragent import UserAgent
+import hashlib
+import os
+import random
+from re import sub
+import requests
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from time import localtime, strftime, sleep
-import hashlib
-import random
-import os
-from discord import Webhook, RequestsWebhookAdapter
-import requests
 import sys
-from re import sub
-from decimal import Decimal
-from dotenv import load_dotenv
+from time import localtime, strftime, sleep
 from urllib.request import urlopen
-from fake_useragent import UserAgent
+
 
 load_dotenv()
 
 #BestBuy Login Info
-loginURL = "https://www.bestbuy.com/identity/global/signin"
-cartURL = "https://bestbuy.com/cart"
-checkoutURL = "https://www.bestbuy.com/checkout/r/fast-track"
-loginEmail = os.getenv('LOGIN_EMAIL')
-loginPass = os.getenv('LOGIN_PASSWORD')
+cart_url = "https://bestbuy.com/cart"
+checkout_url = "https://www.bestbuy.com/checkout/r/fast-track"
 cvv = os.getenv('CVV')
-mentionID = os.getenv('MENTION_ID')
-webhookURL = os.getenv('WEBHOOK_URL')
+login_url = "https://www.bestbuy.com/identity/global/signin"
+login_email = os.getenv('LOGIN_EMAIL')
+login_password = os.getenv('LOGIN_PASSWORD')
+mention_id = os.getenv('MENTION_ID')
+webhook_url = os.getenv('WEBHOOK_URL')
+url_list = os.getenv('URL_LIST').split(", \n")
 
-urlList = os.getenv('URL_LIST').split(", \n")
-
-###Nvidia RTX 3080 FE Best Buy Page URL
-targetURL = "https://www.bestbuy.com/site/nvidia-geforce-rtx-3080-10gb-gddr6x-pci-express-4-0-graphics-card-titanium-and-black/6429440.p?skuId=6429440"
-###Test Page = Mouse Pad
-#targetURLTest = "https://www.bestbuy.com/site/insignia-mouse-pad-black/7536185.p?skuId=7536185"
-targetURLTest = "https://www.bestbuy.com/site/amd-ryzen-7-5800x-4th-gen-8-core-16-threads-unlocked-desktop-processor-without-cooler/6439000.p?skuId=6439000"
-
-#Common XPaths and CSS Selectors
-cssDisabledButton = "c-button-disabled"
-cssAvailableButton = "button.btn-primary:nth-child(1)"
+#target_url_Test = "https://www.bestbuy.com/site/insignia-mouse-pad-black/7536185.p?skuId=7536185"
+target_url_test = "https://www.bestbuy.com/site/amd-ryzen-7-5800x-4th-gen-8-core-16-threads-unlocked-desktop-processor-without-cooler/6439000.p?skuId=6439000"
 
 #Control Variables
-discordPrinting = False
-minWait = 20 #Note that time will be allowed for the page to fully load or time out
-maxWait = 40
-sleepTime = 5
-timeOutLim = 10
-testThreshold = 0 #Set to 0 to run program normally. Set between 1-20 to change frequency of test page loading. Higher = More Likely.
+discord_printing = False
+minimum_wait = 20 #Note that time will be allowed for the page to fully load or time out
+maximum_wait = 40
+sleep_time = 5
+time_out_limit = 10
+test_threshold = 0 #Set to 0 to run program normally. Set between 1-20 to change frequency of test page loading. Higher = More Likely.
 
-###Create webhook for posting updates to Discord.
-webhook = Webhook.from_url(webhookURL, adapter=RequestsWebhookAdapter())
-
-#Set browser as the driver. Configure some settings to help hide the bot from detection and hide driver messages from cluttering terminal.
+#Set browser as the driver. Configure settings to help hide the bot from detection and hide driver messages from cluttering terminal.
 options = webdriver.ChromeOptions()
 options.add_experimental_option("excludeSwitches", ["enable-logging", "enable-automation"])
 options.add_experimental_option("useAutomationExtension", False)
@@ -61,174 +51,181 @@ options.add_argument("disable-blink-features=AutomationControlled")
 driver = webdriver.Chrome(options=options)
 driver.delete_all_cookies()
 
-#Function for printing the same message to discord and terminal. Can be toggled to only print with terminal by setting variable discordPrint = False.
-def TerminalAndDiscordMsg(discordPrint, message, mention=False):
-    print(message)
-    if mention: mentionPre = "<@&" + mentionID + "> "
-    else: mentionPre = ""
-    if discordPrint: webhook.send(mentionPre + message)
 
-######Function to login. Goes to login page, enters e-mail/password and hits Sign In button. Gives time for login redirect to finish before continuing (may not complete login otherwise).
-def loginSeq():
+if discord_printing:
+    webhook = Webhook.from_url(webhook_url, adapter=RequestsWebhookAdapter())
+
+
+def terminal_and_discord_print(discord_printing, message, mention=False):
+    print(message)
+    if mention: 
+        mention_prefix = "<@&" + mention_id + "> "
+    else: 
+        mention_prefix = ""
+    if discord_printing: 
+        webhook.send(mention_prefix + message)
+
+
+def login():
     while True:
-        driver.get(loginURL)
+        driver.get(login_url)
         try:
-                WebDriverWait(driver, sleepTime).until(EC.presence_of_element_located((By.ID, "fld-e"))).send_keys(loginEmail)
-                WebDriverWait(driver, sleepTime).until(EC.presence_of_element_located((By.ID, "fld-p1"))).send_keys(loginPass)
-                WebDriverWait(driver, sleepTime).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".cia-form__controls"))).click()
-                #WebDriverWait(driver, sleepTime).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".btn-secondary"))).click()
-                sleep(timeOutLim)
+                WebDriverWait(driver, sleep_time).until(EC.presence_of_element_located((By.ID, "fld-e"))).send_keys(login_email)
+                WebDriverWait(driver, sleep_time).until(EC.presence_of_element_located((By.ID, "fld-p1"))).send_keys(login_password)
+                WebDriverWait(driver, sleep_time).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".cia-form__controls"))).click()
+                sleep(time_out_limit)
                 print("Logged in successfully.\n\n")
                 break            
         except:
             print("Login fields not detected, retrying.\n")
 
-######Continuous loop to refresh, check stock, and refresh (or exit loop and continue to purchase if stock is detected).
-def reCheck():
+
+def main_loop():
     stock = False
     while stock == False:
-        for url in urlList:
-            if random.randint(0, 20) >= testThreshold: #Random chance of loading the test page.
+        for url in url_list:
+            if random.randint(0, 20) >= test_threshold:
                 while True:
                     try:
                         driver.get(url)
                     except:
                         print("Failed to load page, retrying.")
-                        sleep(sleepTime)
+                        sleep(sleep_time)
                     else: break
             else:
                 while True:
                     try:
-                        driver.get(targetURLTest)
+                        driver.get(target_url_test)
                     except:
                         print("Failed to load page, retrying.")
-                        sleep(sleepTime)
+                        sleep(sleep_time)
                     else: break
-            stock = stockCheck() #Check stock after loading/refreshing page. Will return False if stock is found, True if not found. TRUE = CONTINUE LOOP, FALSE = STOP LOOP
-            if stock == True: break # Break out of for loop if stockCheck() returns True (in stock).
+            stock = check_stock()
+            if stock == True: break
         print("---\n")
-        if stock == True: break # Break out of while loop is stockCheck() returns True (in stock). Bypass random sleep period, goes straight to adding to cart process.
-        sleep(random.randint(minWait, maxWait)) #Sleep a random interval before refreshing tabs. Attempt to make refreshes less robotic.
+        if stock == True: break
+        sleep(random.randint(minimum_wait, maximum_wait))
 
-######Function to determine if there is stock by checking text of button.
-def stockCheck():
-    curTime = strftime("%Y/%m/%d - %I:%M:%S %p", localtime())
-    try: #Try finding the disabled button. If found, print timestamp and out of stock.
-        driver.find_element_by_css_selector(cssDisabledButton).text
-        #TerminalAndDiscordMsg(discordPrinting, "Stock check: " + driver.find_element_by_css_selector(".sku-title").text + "\n~~~ " + curTime + ": Out of Stock\n\n")
-        print("Stock check: " + driver.find_element_by_css_selector(".sku-title").text + "\n~~~ " + curTime + ": Out of Stock\n\n")
-        return False #Returning False will trigger parent loop (reCheck) to continue.
+
+def check_stock():
+    current_time = strftime("%Y/%m/%d - %I:%M:%S %p", localtime())
+    try:
+        driver.find_element(By.CLASS_NAME, "c-button-disabled").text
+        print("Stock check: " + driver.find_element(By.CSS_SELECTOR, ".sku-title").text + "\n~~~ " + current_time + ": Out of Stock\n\n")
+        return False
     except:
-        try: #If disabled button was unable to be located, try finding available button. If found, print "timestamp - in stock" and proceed.
-            driver.find_element_by_css_selector(cssAvailableButton).text
-            TerminalAndDiscordMsg(discordPrinting, "Stock check: " + driver.find_element_by_css_selector(".sku-title").text + "\n~~~ " + curTime + ": IN STOCK\n\n", True)
-            return True #Returning True will break out of parent loop (reCheck).
+        try:
+            if driver.find_element(By.CLASS_NAME, "add-to-cart-button").text == "Add to Cart":
+                terminal_and_discord_print(discord_printing, "Stock check: " + driver.find_element(By.CSS_SELECTOR, ".sku-title").text + "\n~~~ " + current_time + ": IN STOCK\n\n", True)
+                return True
+            else:
+                print("Stock check: " + driver.find_element(By.CSS_SELECTOR, ".sku-title").text + "\n~~~ " + current_time + ": Potentially in stock, but not showing Add to Cart option\n\n")
+                return False
         except: 
-            print("Failed to find button.\n") #If not able to find the disabled button OR available button, print that it failed to find any button
-            return False #Returning False will trigger parent loop (reCheck) to continue.
+            print("Failed to find button.\n")
+            return False
         else: 
             print("ERROR!\n")
-            return False #Returning False will trigger parent loop (reCheck) to continue.
+            return False
 
-######Functions for performing a purchase once in stock.
-def addToCartProcess():
-    buttonElement = driver.find_element_by_css_selector(cssAvailableButton)
-    TerminalAndDiscordMsg(discordPrinting, "=== RUNNING PROCESS: ADD TO CART", True)
-    print("Button says:" + buttonElement.text)
 
-    buttonElement.click()
-    TerminalAndDiscordMsg(discordPrinting, "***Clicked Add to Cart!")
+def add_to_cart():
+    button_element = driver.find_element(By.CLASS_NAME, "add-to-cart-button")
+    terminal_and_discord_print(discord_printing, "=== RUNNING PROCESS: ADD TO CART", True)
+    print("Button says:" + button_element.text)
 
-    afterClickTxt = buttonElement.text
-    print("***Button changed text to: " + afterClickTxt)
+    button_element.click()
+    terminal_and_discord_print(discord_printing, "***Clicked Add to Cart!")
+
+    after_click_text = button_element.text
+    print("***Button changed text to: " + after_click_text)
 
     while True:
         try:
-            #Check if the item has been Added to Cart. Time allowed for this to be displayed is controlled by sleepTime variable. If it is not displayed, keep trying. If it is displayed, proceed with normal buy process.
-            WebDriverWait(driver, sleepTime).until(
+            WebDriverWait(driver, sleep_time).until(
                 EC.presence_of_element_located((By.CSS_SELECTOR, ".continue-shopping")))
-            TerminalAndDiscordMsg(discordPrinting, "***Detected as added to cart.\n\n", True)
-            #sleep(sleepTime)
+            terminal_and_discord_print(discord_printing, "***Detected as added to cart.\n\n", True)
             break
         except: 
             print("Not detected as added to cart. Retrying.")
-            try: buttonElement.click()
+            try: button_element.click()
             except: pass
             print("***Button clicked.\n")
         else: break
 
-######Normal purchasing process.
-def purchProcess():
-    TerminalAndDiscordMsg(discordPrinting, "=== RUNNING PROCESS: PURCHASE")
-    try: #Ignore survey popup to prevent it from interrupting
-                driver.find_element_by_xpath('//*[@id="survey_invite_no"]').click()
+
+def purchase():
+    terminal_and_discord_print(discord_printing, "=== RUNNING PROCESS: PURCHASE")
+    try:
+                driver.find_element(By.XPATH, '//*[@id="survey_invite_no"]').click()
     except: pass
 
     while True:
         try:
-            driver.get(cartURL)
+            driver.get(cart_url)
             print("***Changed page to Cart\n\n")
             False
         except: 
             print("Couldn't load cart, retrying.")
-            sleep(sleepTime)
+            sleep(sleep_time)
             True
         else: break
 
-    driver.find_element_by_css_selector('.checkout-buttons__checkout').click() #Click checkout button.
-    TerminalAndDiscordMsg(discordPrinting, "***Clicked Checkout!")
+    driver.find_element(By.CSS_SELECTOR, ".checkout-buttons__checkout").click()
+    terminal_and_discord_print(discord_printing, "***Clicked Checkout!")
 
     while True:
         try:
-            TerminalAndDiscordMsg(discordPrinting, "WAITING TO CHECKOUT")
+            terminal_and_discord_print(discord_printing, "WAITING TO CHECKOUT")
             #WebDriverWait(driver, sleepTime).until(EC.presence_of_element_located((By.ID, "credit-card-cvv"))).send_keys(cvv)
             #############WebDriverWait(driver, sleepTime).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".btn-lg"))).click()
-            #print("**************Just need to click " + driver.find_element_by_css_selector('.btn-lg').text)
-            #TerminalAndDiscordMsg(discordPrinting, "**************Just need to click " + driver.find_element_by_css_selector('.btn-lg').text)
+            #print("**************Just need to click " + driver.find_element(By.CSS_SELECTOR, ".btn-lg").text)
+            #TerminalAndDiscordMsg(discordPrinting, "**************Just need to click " + driver.find_element(By.CSS_SELECTOR, ".btn-lg").text)
             wait = input("Press any key to exit!")
             system.exit()
         except: pass
         else: break
 
-######Process to skip going to the cart and go straight to checkout page.
-def checkoutProcess():
-    TerminalAndDiscordMsg(discordPrinting, "=== RUNNING PROCESS: CHECKOUT (CART BYPASS)!", True)
+
+def checkout():
+    terminal_and_discord_print(discord_printing, "=== RUNNING PROCESS: CHECKOUT (CART BYPASS)!", True)
 
     while True:
         try:
-            driver.get(checkoutURL)
+            driver.get(checkout_url)
             print("***Changed page to Checkout\n")
             break
         except:
             print("Couldn't load checkout, retrying.")
-            sleep(sleepTime)
+            sleep(sleep_time)
             True
         else: break
     
-    try: #Try switching to shipping instead of pickup. Checkout
-        driver.find_element_by_link_text("Switch to Shipping").click()
-        #sleep(sleepTime)
+    try:
+        driver.find_element(By.LINK_TEXT, "Switch to Shipping").click()
     except: pass
 
-    try: WebDriverWait(driver, sleepTime).under(EC.presence_of_element_located((By.XPATH, '//*[@id="checkoutApp"]/div[2]/div[1]/div[1]/main/div[2]/div[2]/form/section/div/div[2]/div/div/button'))).click()
+    try: WebDriverWait(driver, sleep_time).under(EC.presence_of_element_located((By.XPATH, '//*[@id="checkoutApp"]/div[2]/div[1]/div[1]/main/div[2]/div[2]/form/section/div/div[2]/div/div/button'))).click()
     except: pass
-    sleep(sleepTime)
-    try: WebDriverWait(driver, sleepTime).until(EC.presence_of_element_located((By.ID, "cvv"))).send_keys(cvv) # Attempt to enter CVV
+    sleep(sleep_time)
+    try: WebDriverWait(driver, sleep_time).until(EC.presence_of_element_located((By.ID, "cvv"))).send_keys(cvv) # Attempt to enter CVV
     except: pass
-    try: WebDriverWait(driver, sleepTime).until(EC.presence_of_element_located((By.ID, "credit-card-cvv"))).send_keys(cvv) # Attempt to enter CVV on alternative element ID
+    try: WebDriverWait(driver, sleep_time).until(EC.presence_of_element_located((By.ID, "credit-card-cvv"))).send_keys(cvv) # Attempt to enter CVV on alternative element ID
     except: pass
-    TerminalAndDiscordMsg(discordPrinting, "!!! WAITING TO CHECKOUT", True)
+    terminal_and_discord_print(discord_printing, "!!! WAITING TO CHECKOUT", True)
     
     #############WebDriverWait(driver, sleepTime).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".btn-lg"))).click()
-    #TerminalAndDiscordMsg(discordPrinting, "**************Just need to click " + driver.find_element_by_css_selector('.btn-lg').text)
+    #TerminalAndDiscordMsg(discordPrinting, "**************Just need to click " + driver.find_element(By.CSS_SELECTOR, ".btn-lg").text)
 
     wait = input("Press any key to EXIT!")
     system.exit()
 
-############Initialize program
-TerminalAndDiscordMsg(discordPrinting, "### INITIALIZED: " + strftime("%Y/%m/%d - %I:%M:%S %p", localtime()), True)
 
-loginSeq()
-reCheck()
-addToCartProcess()
-checkoutProcess()
+
+
+terminal_and_discord_print(discord_printing, "### INITIALIZED: " + strftime("%Y/%m/%d - %I:%M:%S %p", localtime()), True)
+
+login()
+main_loop()
+add_to_cart()
+checkout()
